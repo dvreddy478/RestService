e1cf3683a1907512e49a8a404c25a37e2c462711
Manohar <manumca2011@gmail.com>
2016-11-23 15:05:47 +0530

    Added sample connection file

---
+++ src/com/manu/MySqlConnection.java
@@ -0,0 +1,29 @@
+package com.manu;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class MySqlConnection {
+
+	public static void main(String args[]) {
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/impact_db", "root", "lavanya@123");
+			Statement stmt = con.createStatement();
+			
+			ResultSet rs = stmt.executeQuery("select * from user_t");
+			while (rs.next())
+				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
+			con.close();
+		} catch (
+
+		Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
